d31790566f03390fe296cb51455f48114510679d
root <root@srv664135.hstgr.cloud>
2025-10-25 14:03:52 +0500

    fix: Simplify custom view to use standard Filament form rendering

--- resources/views/filament/resources/leads/pages/edit-lead-with-copilot.blade.php
+++ resources/views/filament/resources/leads/pages/edit-lead-with-copilot.blade.php
@@ -2,14 +2,7 @@
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {{-- Main Form (2/3 width) --}}
         <div class="lg:col-span-2">
-            <x-filament-panels::form wire:submit="save">
-                {{ $this->form }}
-
-                <x-filament-panels::form.actions
-                    :actions="$this->getCachedFormActions()"
-                    :full-width="$this->hasFullWidthFormActions()"
-                />
-            </x-filament-panels::form>
+            {{ $this->form }}
         </div>
 
         {{-- AI Copilot Sidebar (1/3 width) --}}
