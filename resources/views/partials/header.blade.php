e02fe151b77a50b3cc32809bbc815dbf68612a4f
root <root@srv664135.hstgr.cloud>
2025-11-10 16:08:14 +0500

    refactor: Remove phone button from navigation header
    - Removed phone CTA button from top navigation
    - Cleaner navigation bar design
    - Phone contact still available in footer
    - Reduces visual clutter in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,13 +15,6 @@
                 <li><a href="{{ url('/contact') }}" class="{{ request()->is('contact') ? 'active' : '' }}">Contact</a></li>
             </ul>
 
-            <a href="tel:+998991234567" class="btn btn--accent nav__cta">
-                <svg width="18" height="18" viewBox="0 0 24 24" aria-hidden="true">
-                    <path d="M6.62 10.79a15.09 15.09 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.24 11.72 11.72 0 003.67.59 1 1 0 011 1v3.54a1 1 0 01-1 1A18.5 18.5 0 013 5a1 1 0 011-1h3.55a1 1 0 011 1 11.72 11.72 0 00.59 3.67 1 1 0 01-.25 1.11z"/>
-                </svg>
-                +998 99 123 4567
-            </a>
-
             <button class="nav__toggle" id="navToggle" aria-label="Toggle navigation menu" aria-expanded="false">
                 <span class="nav__toggle-icon"></span>
             </button>
