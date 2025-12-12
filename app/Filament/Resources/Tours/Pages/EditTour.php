ef806a39801eef4fdf4ab99592ffc8bb51c374dd
root <root@srv664135.hstgr.cloud>
2025-12-12 11:11:03 +0000

    fix: Remove HasWizard trait from EditTour (Filament v4)
    - Edit pages should use Tabs form, not Wizard
    - Removed HasWizard trait, getSteps(), hasSkippableSteps()
    - Removed custom save() method
    - This fixes 500 error when editing tours

--- app/Filament/Resources/Tours/Pages/EditTour.php
+++ app/Filament/Resources/Tours/Pages/EditTour.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Tours\Pages;
 
 use App\Filament\Resources\Tours\TourResource;
-use App\Filament\Resources\Tours\Schemas\TourForm;
 use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
@@ -11,20 +10,11 @@ use Filament\Support\Icons\Heroicon;
 
 class EditTour extends EditRecord
 {
-    use EditRecord\Concerns\HasWizard;
-
     protected static string $resource = TourResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Action::make('save')
-                ->label('Save')
-                ->action(function () {
-                    $this->save(shouldRedirect: false);
-                })
-                ->color('primary')
-                ->icon('heroicon-o-check'),
             Action::make('view_frontend')
                 ->label('View Frontend')
                 ->icon(Heroicon::OutlinedEye)
@@ -38,30 +28,5 @@ class EditTour extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
-    }
-
-    public function getSteps(): array
-    {
-        return TourForm::getWizardSteps();
-    }
-
-    public function save(bool $shouldRedirect = true, bool $shouldSendSavedNotification = true): void
-    {
-        $data = $this->form->getState();
-
-        $this->handleRecordUpdate($this->record, $data);
-
-        if ($shouldSendSavedNotification) {
-            $this->getSavedNotification()->send();
-        }
-
-        if ($shouldRedirect) {
-            $this->redirect($this->getRedirectUrl());
-        }
-    }
-
-    public function hasSkippableSteps(): bool
-    {
-        return true;
     }
 }
