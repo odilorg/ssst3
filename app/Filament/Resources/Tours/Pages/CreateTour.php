046e769a774ef4e04833020764915e0600668a5a
root <root@srv664135.hstgr.cloud>
2025-12-12 11:15:27 +0000

    feat: Switch Create page from Wizard to Tabs form
    - Removed HasWizard trait from CreateTour
    - Both Create and Edit now use same Tabs form
    - Simpler, more consistent UX
    - Fixes wizard disappearing issue

--- app/Filament/Resources/Tours/Pages/CreateTour.php
+++ app/Filament/Resources/Tours/Pages/CreateTour.php
@@ -3,30 +3,14 @@
 namespace App\Filament\Resources\Tours\Pages;
 
 use App\Filament\Resources\Tours\TourResource;
-use App\Filament\Resources\Tours\Schemas\TourForm;
 use Filament\Resources\Pages\CreateRecord;
-use Filament\Forms\Components\Wizard;
-use Filament\Schemas\Schema;
-use Filament\Actions\Action;
 
 class CreateTour extends CreateRecord
 {
-    use CreateRecord\Concerns\HasWizard;
-
     protected static string $resource = TourResource::class;
 
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
-    }
-
-    public function getSteps(): array
-    {
-        return TourForm::getWizardSteps();
-    }
-
-    public function hasSkippableSteps(): bool
-    {
-        return true;
     }
 }
